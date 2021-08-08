25b53c8cd3ef57c8e5d50b3b837aa7046d48bcb9
dolev <dolevizacson@gmail.com>
2021-08-08 20:44:50 +0300

    try to fix heroku bug

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    bufferLogs: true,
+    // bufferLogs: true,
   });
   app.useLogger(app.get(Logger));
-  await app.listen(process.env.PORT || 8000, '0.0.0.0');
+  await app.listen(process.env.PORT || 8000);
   app.get(Logger).log(`app listen on port ${process.env.PORT || 8000}`, 'Main');
 }
 bootstrap();
