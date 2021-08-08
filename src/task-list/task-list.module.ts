b3d1e974c472d501b7e460b5ec4aa3bad8067277
dolev <dolevizacson@gmail.com>
2021-08-08 18:29:09 +0300

    add task list entity to task list module

--- src/task-list/task-list.module.ts
+++ src/task-list/task-list.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TaskListService } from './task-list.service';
 import { TaskListController } from './task-list.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskList } from './entities/task-list.entity';
 
 @Module({
-  controllers: [TaskListController],
+  imports: [TypeOrmModule.forFeature([TaskList])],
   providers: [TaskListService],
+  controllers: [TaskListController],
 })
 export class TaskListModule {}
