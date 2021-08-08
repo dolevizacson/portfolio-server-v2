4f8d5db9d353de82d86ae77165592709487546e0
dolev <dolevizacson@gmail.com>
2021-08-08 18:33:21 +0300

    add properties to task list entity

--- src/task-list/entities/task-list.entity.ts
+++ src/task-list/entities/task-list.entity.ts
@@ -1,1 +1,16 @@
-export class TaskList {}
+import { Column, Entity, ObjectID, ObjectIdColumn } from 'typeorm';
+
+@Entity()
+export class TaskList {
+  @ObjectIdColumn()
+  id: ObjectID;
+
+  @Column()
+  header: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  isDone: number;
+}
