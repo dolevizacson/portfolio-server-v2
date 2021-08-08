228550f2e328ea0acff5d9b277d6cb1630bd2300
dolev <dolevizacson@gmail.com>
2021-08-08 18:23:16 +0300

    change nest logger to pino logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(Logger));
+  await app.listen(process.env.PORT || 8000, () => {
+    app
+      .get(Logger)
+      .log(`app listen on port ${process.env.PORT || 8000}`, 'Main');
+  });
 }
 bootstrap();
