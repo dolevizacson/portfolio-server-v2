88afd118adb2a7c942c527e468f18e3a25526f46
dolev <dolevizacson@gmail.com>
2021-08-08 18:22:33 +0300

    add config, logger and typeorm modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskListModule } from './task-list/task-list.module';
+import envVarValidationSchema from './config/envVar.schema';
+import { LoggerModule, Params } from 'nestjs-pino';
+import loggerConfig from './config/logger.config';
+import mongodbConfig from './config/mongodb.config';
+import { MongoConnectionOptions } from 'typeorm/driver/mongodb/MongoConnectionOptions';
 
 @Module({
-  imports: [TaskListModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envVarValidationSchema,
+      load: [loggerConfig, mongodbConfig],
+    }),
+    LoggerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return configService.get<Params>('loggerConfiguration');
+      },
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return configService.get<MongoConnectionOptions>(
+          'mongoConnectionSetting',
+        );
+      },
+    }),
+    TaskListModule,
+  ],
   controllers: [],
   providers: [],
 })
