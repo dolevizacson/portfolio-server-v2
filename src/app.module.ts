17e03f39c5036a3dd0087c87b9bf9f37b798ebc4
dolev <dolevizacson@gmail.com>
2021-09-09 22:32:39 +0300

    add modules to root app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,37 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
+
 import { TaskListModule } from './task-list/task-list.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ProjectsModule } from './projects/projects.module';
 import envVarValidationSchema from './config/envVar.schema';
 import mongodbConfig from './config/mongodb.config';
-import { MongoConnectionOptions } from 'typeorm/driver/mongodb/MongoConnectionOptions';
+import adminCredentialsConfig from './config/admin-credentials.config';
+import jwtOptionsConfig from './config/jwt-options.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: envVarValidationSchema,
-      load: [mongodbConfig],
+      load: [mongodbConfig, adminCredentialsConfig, jwtOptionsConfig],
     }),
-    TypeOrmModule.forRootAsync({
+    MongooseModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
-        return configService.get<MongoConnectionOptions>(
+        return configService.get<MongooseModuleOptions>(
           'mongoConnectionSetting',
         );
       },
     }),
     TaskListModule,
+    UsersModule,
+    AuthModule,
+    ProjectsModule,
   ],
-  controllers: [],
   providers: [],
+  controllers: [],
 })
 export class AppModule {}
