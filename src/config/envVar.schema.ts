25a0c568db2fce192e782dfd2179c483d50ec9cd
dolev <dolevizacson@gmail.com>
2021-09-09 22:09:55 +0300

    add environment variables to validation schema

--- src/config/envVar.schema.ts
+++ src/config/envVar.schema.ts
@@ -2,4 +2,8 @@ import * as Joi from 'joi';
 
 export default Joi.object({
   MONGODB_URL: Joi.string().required(),
+  ADMIN_USER_NAME: Joi.string(),
+  ADMIN_PASSWORD: Joi.string(),
+  JWT_SECRET: Joi.string().required(),
+  JWT_EXP: Joi.number().required(),
 });
