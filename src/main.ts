7db702a904a1f1d10b9e84cebe6263087523e476
dolev <dolevizacson@gmail.com>
2021-09-09 22:28:52 +0300

    add spaces

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 
+import { AppModule } from './app.module';
 import { LoggingInterceptor } from './common/Interceptors/RouteLogger.interceptor';
 
 async function bootstrap() {
   const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule, {});
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LoggingInterceptor());
+
   await app.listen(process.env.PORT || 8000);
+
   logger.log(`app listen on port ${process.env.PORT || 8000}`);
 }
 bootstrap();
