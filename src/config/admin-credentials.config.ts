448b6fd56601d25fc00cc5d78136c54b3f75b81f
dolev <dolevizacson@gmail.com>
2022-01-27 18:59:22 +0200

    add type safety to admin config

--- src/config/admin-credentials.config.ts
+++ src/config/admin-credentials.config.ts
@@ -1,4 +1,4 @@
-export default () => ({
+export default (): { adminUserName: string; adminPassword: string } => ({
   adminUserName: process.env.ADMIN_USER_NAME,
   adminPassword: process.env.ADMIN_PASSWORD,
 });
